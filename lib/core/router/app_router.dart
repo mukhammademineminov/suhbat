818d003930c4d11453c2613eb2c787cc1777b4aa
mukhammademin <mukhammademin19@icloud.com>
2026-05-11 21:35:08 +0500

    feat: add auth state listener for auto-redirect on login/logout

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,13 +1,38 @@
-//import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:suhbat/features/auth/presentation/login_screen.dart';
 import 'package:suhbat/features/auth/presentation/register_screen.dart';
 import 'package:suhbat/features/chat/presentation/rooms_screen.dart';
 
+import 'package:flutter/material.dart';
+
+class AuthChangeNotifier extends ChangeNotifier {
+  AuthChangeNotifier() {
+    Supabase.instance.client.auth.onAuthStateChange.listen((_) {
+      notifyListeners();
+    });
+  }
+}
+
+final _authNotifier = AuthChangeNotifier();
 
 final router = GoRouter(
   initialLocation: '/login',
+  refreshListenable: _authNotifier,
+  redirect: (context, state) {
+    final session = Supabase.instance.client.auth.currentSession;
+    final isLoggedIn = session != null;
+    final isAuthRute = state.matchedLocation == '/login' || state.matchedLocation == '/register';
+
+    if (isLoggedIn && isAuthRute) {
+      return '/rooms_chat';
+    }
+    if (!isLoggedIn && !isAuthRute) {
+      return '/login';
+    }
+    return null;
+  },
   routes: [
     GoRoute(
       path: '/login',
--- lib/features/chat/presentation/rooms_screen.dart
+++ lib/features/chat/presentation/rooms_screen.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'package:suhbat/core/router/app_router.dart';
+//import 'package:suhbat/core/router/app_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-class RoomsScreen  extends StatelessWidget {
+class RoomsScreen extends StatelessWidget {
   const RoomsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Rooms'),
-      ),
-      body: const Center(
-        child: Text('Welcome to Chat Rooms'),
+      appBar: AppBar(title: const Text('Rooms')),
+      body: Center(
+        child: TextButton(
+          onPressed: () async {
+            await Supabase.instance.client.auth.signOut();
+            
+          },
+          child: Text('Logout'),
+        ),
       ),
     );
   }
